361765cb154f2f317eab9070a24c2a0eb761bc00
Nicklow <Allison.Nicklow@stlcop.local>
2017-10-30 14:44:39 -0500

    number 3 all 5 plots

--- Number3.R
+++ Number3.R
@@ -6,3 +6,34 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
 #rest of your code goes here
+#plot 1
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  labs(x= "Adult Mass", y="Baby Mass") +
+  geom_point()
+#plot2
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  labs(x= "Adult Mass", y="Baby Mass") +
+  scale_x_log10() +
+  scale_y_log10() +
+  geom_point()
+#plot 3
+ggplot(data = dat, aes(x = mass.g., y = newborn.g., color = order)) +
+  labs(x= "Adult Mass", y="Baby Mass") +
+  scale_x_log10() +
+  scale_y_log10() +
+  geom_point()
+#plot4
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.,color = order)) +
+  labs(x= "Adult Mass", y="Baby Mass") +
+  scale_x_log10() +
+  scale_y_log10() +
+  geom_point() +
+  facet_wrap(~order, nrow = 4, ncol = 5)
+#plot 5
+ggplot(data = dat, aes(x = mass.g., y = newborn.g., color=order)) +
+  labs(x= "Adult Mass", y="Baby Mass") +
+  scale_x_log10() +
+  scale_y_log10() +
+  geom_point() +
+  geom_smooth(method = "lm")+
+  facet_wrap(~order, nrow = 4,ncol = 5)
